95d6936555524f0a1d5c41280825cd1fd830d43e
robertcjoshy <robertcjoshy11@gmail.com>
2024-04-18 23:50:59 +0530

    code

--- router/routes.go
+++ router/routes.go
@@ -5,9 +5,14 @@ import (
 	//"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
 	"github.com/robert/middelwares/handler"
+	"github.com/robert/middelwares/middleware"
 )
 
 func Startserver() {
 	r := gin.Default()
-	r.GET("/login", handler.Login)
+	r.LoadHTMLGlob("templates/*.html")
+	r.GET("/", middleware.Authmiddleware(), handler.Mainpage)
+	r.GET("/login", handler.Loginget)
+	r.POST("/login", handler.Loginpost)
+	r.Run()
 }
