129091aea06fe0bddfac8f18062a012f852d9e90
robertcjoshy <robertcjoshy11@gmail.com>
2024-04-18 23:50:42 +0530

    code

--- handler/handlers.go
+++ handler/handlers.go
@@ -6,6 +6,22 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func Login(c *gin.Context) {
+func Mainpage(c *gin.Context) {
+	c.HTML(http.StatusOK, "index.html", gin.H{})
+}
+
+func Loginget(c *gin.Context) {
 	c.HTML(http.StatusOK, "login.html", gin.H{})
+}
+
+func Loginpost(c *gin.Context) {
+
+	username := c.PostForm("username")
+	password := c.PostForm("password")
+
+	if username == "r" && password == "1" {
+		c.Redirect(http.StatusFound, "/")
+	} else {
+		c.HTML(http.StatusBadRequest, "login.html", gin.H{"message": "invalid email or password"})
+	}
 }
